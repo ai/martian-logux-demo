801a2fc90ec52dd4982ff21c0f834fbef5ba5de2
Andrey Sitnik <andrey@sitnik.ru>
2021-10-02 20:35:45 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+export function App() {
+  return null
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
